60c93453ffcd2c011184c5823f0947f28f79d898
Rogerio Carvalho <bb.roger.bb@gmail.com>
2020-09-28 17:27:57 -0300

    teste de fontes

--- App.js
+++ App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { View, Button, Text, Grid, Col } from 'native-base';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf'),
+    'roboto-italic': require('./assets/fonts/Roboto-Italic.ttf'),
+    'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf')
+  });
+};
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    )
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+      <Text style={{ fontFamily: 'roboto-bold' }}>
+        Olá mundo react!!
+      </Text>
+      <Text style={{ fontFamily: 'roboto-italic' }}>
+        Olá mundo react!!
+      </Text>
+      <Text style={{ fontFamily: 'roboto-regular' }}>
+        Olá mundo react!!
+      </Text>
+      <Button>
+        <Text>Login</Text>
+      </Button>
+    </View >
   );
 }
 
