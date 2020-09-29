ad92ca563231908c096cb7113e698a0367324621
Rogerio Carvalho <bb.roger.bb@gmail.com>
2020-09-29 18:48:17 -0300

    inclusão do layout dos produtos

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { View, Button, Text, Grid, Col } from 'native-base';
+import React, { useState, Fragment } from 'react';
+import { StyleSheet, Image } from 'react-native';
+import { Container, Header, Left, Right, Body, Title, Button, Icon, View, Fab, List, ListItem, Thumbnail, Text, Badge, Content, Tab, Tabs, TabHeading, Card, CardItem } from 'native-base';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 
@@ -12,6 +12,52 @@ const fetchFonts = () => {
     'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf')
   });
 };
+
+const products = [
+  {
+    "id": 1,
+    "title": "Brown eggs",
+    "type": "dairy",
+    "description": "Raw organic brown eggs in a basket",
+    "filename": "0.jpg",
+    "height": 600,
+    "width": 400,
+    "price": 28.1,
+    "rating": 4
+  }
+];
+
+const Home = () => (
+  <Content>
+    {products.map(product => (
+      <Card key={product.id}>
+        <CardItem>
+          <Left>
+            <Thumbnail />
+            <Body>
+              <Text>{product.title}</Text>
+            </Body>
+          </Left>
+        </CardItem>
+        <CardItem cardBody>
+          <Image style={{ height: 200, width: null, flex: 1 }} />
+        </CardItem>
+        <CardItem>
+          <Left>
+            <Button transparent>
+              <Icon active name="thumbs-up" />
+              <Text>{product.rating}</Text>
+            </Button>
+          </Left>
+          <Right>
+            <Text note>{product.price}</Text>
+          </Right>
+        </CardItem>
+      </Card>))}
+  </Content>
+);
+const Messages = () => null;
+const Notification = () => null;
 
 export default function App() {
   const [dataLoaded, setDataLoaded] = useState(false);
@@ -24,30 +70,39 @@ export default function App() {
       />
     )
   }
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text style={{ fontFamily: 'roboto-bold' }}>
-        Olá mundo react!!
-      </Text>
-      <Text style={{ fontFamily: 'roboto-italic' }}>
-        Olá mundo react!!
-      </Text>
-      <Text style={{ fontFamily: 'roboto-regular' }}>
-        Olá mundo react!!
-      </Text>
-      <Button>
-        <Text>Login</Text>
-      </Button>
-    </View >
+    <Container>
+      <Header androidStatusBarColor="#573ea8" style={styles.header} hasTabs>
+        <Body>
+          <Title>Store</Title>
+        </Body>
+      </Header>
+      <View style={styles.container}>
+        <Tabs>
+          <Tab heading={<TabHeading style={styles.tabHeading} ><Icon type="FontAwesome" name="home" /></TabHeading>}>
+            <Home />
+          </Tab>
+          <Tab heading={<TabHeading style={styles.tabHeading} ><Icon type="FontAwesome" name="bell-o" /></TabHeading>}>
+            <Notification />
+          </Tab>
+          <Tab heading={<TabHeading style={styles.tabHeading} ><Icon type="FontAwesome" name="envelope-o" /></TabHeading>}>
+            <Messages />
+          </Tab>
+        </Tabs>
+      </View>
+    </Container>
   );
 }
 
 const styles = StyleSheet.create({
+  tabHeading: {
+    backgroundColor: "blue",
+  },
+  header: {
+    backgroundColor: "#00008B",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
